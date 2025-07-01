e43ae2344a4c44b3a42f99cecdf193d136787ec3
Soubhagya Nayak <155530626+Soubhagya-Nayak@users.noreply.github.com>
2025-07-01 21:39:30 +0530

    adding new files for understanding about variable inject, function call etc.

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+function MyApp() {
+  return(
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <MyApp />
 )
