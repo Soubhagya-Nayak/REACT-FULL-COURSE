bf45190064d712bbac1f07a76e12afdd3afddeff
Soubhagya Nayak <155530626+Soubhagya-Nayak@users.noreply.github.com>
2025-07-10 17:30:37 +0530

    adding TODO List project and refactoring ToggleSwitch component

--- myReactApp/src/App.jsx
+++ myReactApp/src/App.jsx
@@ -1,18 +1,19 @@
 // import NetflixSerises from "./components/NetflixSerises";
 // import "./components/Netflix.css";
 // import "./components/Netflix.module.css";
-import "./components/EV.css";
+// import "./components/EV.css";
 // import { EventHandling } from "./components/EventHandling";
 // import { EventProps } from "./components/EventProps";
 // import { EventPropagation } from "./components/EventPropagation";
 // import { States } from "./components/hooks/States";
 // import { DerivedState } from "./components/DerivedState";
 // import { LiftingState } from "./components/LiftingStateUp";
-import { ToggleSwitch } from "./project1/ToggleSwitch";
+// import { ToggleSwitch } from "./projects/project1/ToggleSwitch";
+import { Todo } from "./projects/TODO-List/Todo";
 
 export const App = () => {
   return (
-    <section className="container">
+    <section>
       {/* <h1 className="card-header">Best Series in Netflix</h1> */}
       {/* <NetflixSerises /> */}
       {/* <h3 className="card-footer">All Copyright reserved @ sourcebysoubh | 2025</h3> */}
@@ -22,7 +23,8 @@ export const App = () => {
       {/* <States /> */}
       {/* <DerivedState /> */}
       {/* <LiftingState /> */}
-      <ToggleSwitch />
+      {/* <ToggleSwitch /> */}
+      <Todo />
     </section>
   );
 };
---
+++ myReactApp/src/projects/TODO-List/Todo.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./Todo.css";
+import { MdCheckCircle, MdDelete } from "react-icons/md";
+
+export const Todo = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [task, setTask] = useState([]);
+
+    const handleInputChange = (value) => {
+        setInputValue(value);
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        if (!inputValue) return;
+
+        if (task.includes(inputValue)) {
+            setInputValue("");
+            return;
+        }
+
+        setTask((prevTask) => [...prevTask, inputValue]);
+
+        setInputValue("");
+    }
+    return (
+        <section className="todo-container">
+            <header>
+                <h1>TODO List</h1>
+            </header>
+
+            <section className="form">
+                <form onSubmit={handleFormSubmit}>
+                    <div>
+                        <input type="text" className="todo-input" autoComplete="off" value={inputValue} onChange={(event) => handleInputChange(event.target.value)} />
+                    </div>
+                    <div>
+                        <button type="submit" className="todo-btn">Add Task</button>
+                    </div>
+                </form>
+            </section>
+
+            <section className="myUnOrdList">
+                <ul>
+                    {
+                        task.map((curTask, index) => {
+                            return (
+                                <li key={index}>
+                                    <span>{curTask}</span>
+                                    <button className="check-btn"><MdCheckCircle /></button>
+                                    <button className="delete-btn"><MdDelete /></button>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </section>
+        </section>
+    )
+}
