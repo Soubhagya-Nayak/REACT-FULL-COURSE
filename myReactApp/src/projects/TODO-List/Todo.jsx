e508f23bfc9f2c65805d4755d4f085f06bbe7f8d
Soubhagya Nayak <155530626+Soubhagya-Nayak@users.noreply.github.com>
2025-07-12 11:12:31 +0530

    adding new files in todo list project and modifying existing files

--- myReactApp/src/projects/TODO-List/Todo.jsx
+++ myReactApp/src/projects/TODO-List/Todo.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Todo.css";
 import { MdCheckCircle, MdDelete } from "react-icons/md";
 
 export const Todo = () => {
     const [inputValue, setInputValue] = useState("");
     const [task, setTask] = useState([]);
+    const [dateTime, setDateTime] = useState("");
 
     const handleInputChange = (value) => {
         setInputValue(value);
     }
 
+    // Todo input submit
     const handleFormSubmit = (event) => {
         event.preventDefault();
 
@@ -24,10 +26,39 @@ export const Todo = () => {
 
         setInputValue("");
     }
+
+
+    // Todo Date and Time
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            const formattedDate = now.toLocaleDateString();
+            const formattedTime = now.toLocaleTimeString();
+
+            setDateTime(`${formattedDate} - ${formattedTime}`);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+
+    // Todo handleDeleteTodo function
+    const handleDeleteTodo = (value) => {
+        console.log(task);
+        console.log(value);
+        const updatedTask = task.filter((curTask) => curTask !== value);
+        setTask(updatedTask);
+    }
+
+    // Todo handleClearButton function
+    const handleClearButton = () => {
+        setTask([]);
+    };
+
     return (
         <section className="todo-container">
             <header>
                 <h1>TODO List</h1>
+                <h2 className="date-time">{dateTime}</h2>
             </header>
 
             <section className="form">
@@ -46,15 +77,19 @@ export const Todo = () => {
                     {
                         task.map((curTask, index) => {
                             return (
-                                <li key={index}>
+                                <li key={index} className="todo-item">
                                     <span>{curTask}</span>
                                     <button className="check-btn"><MdCheckCircle /></button>
-                                    <button className="delete-btn"><MdDelete /></button>
+                                    <button className="delete-btn" onClick={() => handleDeleteTodo(curTask)}><MdDelete /></button>
                                 </li>
                             )
                         })
                     }
                 </ul>
+            </section>
+
+            <section>
+                <button className="clear-btn" onClick={handleClearButton}>Clear All</button>
             </section>
         </section>
     )
